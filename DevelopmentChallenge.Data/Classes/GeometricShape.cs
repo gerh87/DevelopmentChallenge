69bd68b388634e9857850c442216a366030cf872
Pablo Germán <pablo_gh87@hotmail.es>
2024-11-20 01:33:55 -0300

    Agregar archivos de proyecto.

---
+++ DevelopmentChallenge.Data/Classes/GeometricShape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopmentChallenge.Data.Classes
+{
+    public abstract class GeometricShape
+    {
+        public decimal Side { get; set; }
+
+        public abstract decimal CalculateArea();
+        public abstract decimal CalculatePerimeter();
+    }
+}
---
+++ DevelopmentChallenge.Data/Classes/Square.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopmentChallenge.Data.Classes
+{
+    public class Square : GeometricShape
+    {
+        public override decimal CalculateArea()
+        {
+            return Side * Side;
+        }
+
+        public override decimal CalculatePerimeter()
+        {
+            return Side * 4;
+        }
+    }
+}
